7e4aa28bd21a8699a1c390cd5d8bace3d0afad88
Лиза <elismosh@inbox.ru>
2021-04-20 20:31:24 +0300

    it works! (todo: send_photo)

--- Country_class.py
+++ Country_class.py
@@ -1,4 +1,5 @@
 import requests
+import sqlite3
 
 
 class Country:
@@ -10,29 +11,45 @@ class Country:
         self.set_pos()
 
     def set_name(self):
-        pass
+        con = sqlite3.connect("countries.db")
+        cur = con.cursor()
+        self.name = cur.execute(f"""SELECT country FROM paths
+                    WHERE id = {self.id}""").fetchone()
+        con.close()
 
     def set_path(self):
-        pass
+        con = sqlite3.connect("countries.db")
+        cur = con.cursor()
+        self.path = cur.execute(f"""SELECT path FROM paths
+                            WHERE id = {self.id}""").fetchone()
+        con.close()
 
     def set_pos(self):
-        self.x = 0
-        self.y = 0
+        map_request = f"https://geocode-maps.yandex.ru/1.x/?apikey=40d1649f-0493-4b70-98ba-98533de7710b&" \
+                      f"geocode={self.name}&format=json"
+        response = requests.get(map_request)
+        json_response = response.json()
+        toponym = json_response["response"]["GeoObjectCollection"]["featureMember"][0]["GeoObject"]
+        self.x = float(toponym["Point"]["pos"].split()[0])
+        self.y = float(toponym["Point"]["pos"].split()[1])
 
     def right_country(self, text):
         map_request = f"https://geocode-maps.yandex.ru/1.x/?apikey=40d1649f-0493-4b70-98ba-98533de7710b&" \
                       f"geocode={text}&format=json"
         response = requests.get(map_request)
 
-        if not response:
-            return 0  # Нет такого адресса
-        json_response = response.json()
-        toponym = json_response["response"]["GeoObjectCollection"]["featureMember"][0]["GeoObject"]
-        object = toponym["metaDataProperty"]["GeocoderMetaData"]
+        # if not response:
+        #     return 0  # Нет такого адресса
+        try:
+            json_response = response.json()
+            toponym = json_response["response"]["GeoObjectCollection"]["featureMember"][0]["GeoObject"]
+            object = toponym["metaDataProperty"]["GeocoderMetaData"]
+        except Exception:
+            return 0
         if object['kind'] != "country":
             return 1  # Это не страна
-        x = toponym["Point"]["pos"].split()[0]
-        y = toponym["Point"]["pos"].split()[1]
+        x = float(toponym["Point"]["pos"].split()[0])
+        y = float(toponym["Point"]["pos"].split()[1])
         if x == self.x and y == self.y:
             return 2  # Верно отгадано
         if abs(x - self.x) >= abs(y - self.y):
@@ -41,7 +58,7 @@ class Country:
             else:
                 return 32  # восток
         else:
-            if y > self.y:
+            if y < self.y:
                 return 33  # север
             else:
                 return 34  # юг
--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import random
+import sqlite3
 import sys
 
 import requests
@@ -9,60 +10,64 @@ from Country_class import Country
 
 TOKEN = '1772095252:AAGf3sq-39Uvu2XzAscP-qDuHP2J31fQREE'
 country_outline = False
-pasta = ''
-attemps = 10
+pasta = 'рофл'
+attemps = 100
+outline = Country(random.randrange(1, 46), attemps)
 
 
 # Определяем функцию-обработчик сообщений.
 # У неё два параметра, сам бот и класс updater, принявший сообщение.
+
 def reply(update, context):
-    global country_outline
+    global country_outline, attemps, outline
     text = update.message.text
-    # update.message.reply_text(right_country(text))
     if country_outline:
         exit_code = outline.right_country(text)
+        rep_list = []
         if exit_code == 0:
-            pass
-        # Нет такого адреса. Я не знаю такого адреса. Вы ввели неправильные данные.
+            rep_list = ['Нет такого адреса.', 'Я не знаю такого адреса.', 'Вы ввели неверные данные.']
         elif exit_code == 1:
-            pass
-        # Это не страна. Я не знаю такой страны.
+            rep_list = ['Это не страна.', 'Я не знаю такой страны.']
         elif exit_code == 2:
-            pass
-        # Верно! Молодец, это правильный ответ! Правильно!
-        # Вам понадобилось   попыток!
-        elif exit_code // 10 == 3:
+            country_outline = False
+            rep_list = ['Верно!', 'Это правильный ответ!', 'Правильно!']
+            update.message.reply_text(rep_list[random.randrange(len(rep_list))])
+            update.message.reply_text(f'Вам понадобилось {attemps - outline.attemps} попыток!')
+            return
+        else:
             outline.attemps -= 1
             if exit_code == 31:
-                pass
-            # Загаданная страна западнее! Западнее! Думай шире, я загадал более западную страну.
+                rep_list = ['Загаданная страна западнее!', 'Западнее!', 'Думай шире, я загадал более западную страну.']
             elif exit_code == 32:
-                pass
-            # Загаданная страна восточнее! Восточнее! Думай шире, я загадал более восточную страну.
+                rep_list = ['Загаданная страна восточнее!', 'Восточнее!',
+                            'Думай шире, я загадал более восточную страну.']
             elif exit_code == 33:
-                pass
-            # Загаданная страна севернее! Севернее! Думай шире, я загадал более северную страну.
+                rep_list = ['Загаданная страна севернее!', 'Севернее!',
+                            'Думай шире, я загадал более северную страну.']
             elif exit_code == 34:
-                pass
-            # Загаданная страна южнее! Южнее! Думай шире, я загадал более южную страну.
+                rep_list = ['Загаданная страна южнее!', 'Южнее!',
+                            'Думай шире, я загадал более южную страну.']
+        if outline.attemps > 0:
+            update.message.reply_text(rep_list[random.randrange(len(rep_list))])
+            update.message.reply_text(f'У Вас осталось {outline.attemps} попыток!')
     else:
         update.message.reply_text('Извините, я не знаю такой команды.')
         update.message.reply_text(pasta)
     if outline.attemps == 0:
         country_outline = False
+        update.message.reply_text('У Вас осталось 0 попыток!')
+        update.message.reply_text(['Вы проиграли!', 'К сожалению, Вы проиграли.'][random.randrange(2)])
+        update.message.reply_text(f'Это {outline.name}! Вы были близки!')
 
 
 def start_country_outline(update, context):
-    global outline
-    update.message.reply_text("Привет! Я эхо-бот. Напишите мне что-нибудь, и я пришлю это назад!")
-    outline = Country(random.randrange(1, 65), attemps)
+    global outline, country_outline
+    update.message.reply_text("Начинаем игру!")
 
-
-def end_country_outline(update, context):
-    global outline
-    country_outline = False
-    update.message.reply_text("Привет! Я эхо-бот. Напишите мне что-нибудь, и я пришлю это назад!")
-    outline = Country(random.randrange(1, 65), attemps)
+    outline = Country(random.randrange(1, 2), attemps)
+    update.message.reply_text("Вот очертания загаданной страны:")
+    update.bot.send_photo(chat_id='@GuessPlace_bot', photo=open(outline.path, 'rb'))
+    country_outline = True
 
 
 def help(update, context):
@@ -95,3 +100,4 @@ if __name__ == '__main__':
 # https://static-maps.yandex.ru/1.x/?ll=16.106443,45.439828&spn=0.9,0.9&l=map&lang=ht_TR
 # 0 - 179.999
 # - 83
+# https://geocode-maps.yandex.ru/1.x/?apikey=40d1649f-0493-4b70-98ba-98533de7710b&geocode=hbsd&format=json
